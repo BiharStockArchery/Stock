442127285d2765dcf3cbd9e7d32c044a5c97b0d5
BiharStockArchery <stockarcher.bihar@gmail.com>
2024-08-15 10:45:04 +0530

    bdhjddghjdkdh

--- src/components/sectionpage.jsx
+++ src/components/sectionpage.jsx
@@ -140,6 +140,11 @@ const SectionsPage = () => (
         heading={<><strong>Comprehensive Trading Experience</strong></>}
         text="A unique blend of theoretical knowledge and practical execution, ensuring well-rounded trading expertise."
       />
+       <SectionComponent
+        image="./images/in.jpeg"
+        heading={<><strong>Interactive Learning Enviroment</strong></>}
+        text=" Engage in an energetic and collaborative setting where real-time discussions and peer interactions enhance the learning experience."
+      />
     </Row>
   </Container>
 );
