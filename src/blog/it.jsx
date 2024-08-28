937ed98a77c1aa8b08558aa69c21a464f7fad2af
BiharStockArchery <stockarcher.bihar@gmail.com>
2024-08-28 13:49:45 +0530

    vf

--- src/blog/it.jsx
+++ src/blog/it.jsx
@@ -6,7 +6,7 @@ const styles = {
       overflowX: 'hidden',
     },
     header: {
-      backgroundImage: 'url(https://res.cloudinary.com/dyrn2eg1j/image/upload/v1724832059/1_djrz1r.jpg)', // Replace with your image URL
+      backgroundImage: 'url(https://res.cloudinary.com/dyrn2eg1j/image/upload/v1724832913/Screenshot_14_s1rlca.png)', // Replace with your image URL
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       color: 'white',
--- src/components/blogpage.jsx
+++ src/components/blogpage.jsx
@@ -25,7 +25,7 @@ const BlogPage = () => {
         {
             title: "IT Stocks Propel Nifty to Fresh All-Time High; Sensex Surges Over 250 Points",
             description: "The Indian stock market witnessed a significant surge today as the Nifty 50 index reached a fresh all-time high, driven primarily by gains in IT stocks. ",
-            imgUrl: "https://res.cloudinary.com/dyrn2eg1j/image/upload/v1724832059/1_djrz1r.jpg",
+            imgUrl: "https://res.cloudinary.com/dyrn2eg1j/image/upload/v1724832913/Screenshot_14_s1rlca.png",
             route: "/it",
         },
     ];
