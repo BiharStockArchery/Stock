cd1ed13f92157880daa6051b56aca0fce71e03e3
BiharStockArchery <stockarcher.bihar@gmail.com>
2024-08-30 15:27:51 +0530

    f

--- src/components/firebase2.js
+++ src/components/firebase2.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, addDoc } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyDTBEZLNKhhYdenBtIxXayyaDirONWE7XI",
-    authDomain: "seprate-database.firebaseapp.com",
-    projectId: "seprate-database",
-    storageBucket: "seprate-database.appspot.com",
-    messagingSenderId: "827847101777",
-    appId: "1:827847101777:web:206fe9a75b53b4560f5770"
+    apiKey: "AIzaSyDzjEPHH4h8lqbBpfPMt8BiiG7v1A5QY1Y",
+    authDomain: "contact-e6380.firebaseapp.com",
+    databaseURL: "https://contact-e6380-default-rtdb.firebaseio.com",
+    projectId: "contact-e6380",
+    storageBucket: "contact-e6380.appspot.com",
+    messagingSenderId: "331095978266",
+    appId: "1:331095978266:web:d8f9a3e59541b9032efca3"
 };
 
 // Initialize Firebase
