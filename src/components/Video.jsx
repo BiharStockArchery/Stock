1f86aa3ace0d01de8f36ef960490cc9b4e485fcb
BiharStockArchery <stockarcher.bihar@gmail.com>
2024-09-12 11:37:10 +0530

    iu

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -71,7 +71,7 @@ const Video = () => {
     <Container>
       <VideoWrapper>
         <iframe
-          src="https://www.youtube.com/embed/stxCGM-XAP0"
+          src="https://www.youtube.com/embed/MSIvu4-2WeY"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
