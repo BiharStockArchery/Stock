d2ab4af72f32f832f663b0ab7560f4a983646df4
BiharStockArchery <stockarcher.bihar@gmail.com>
2024-09-13 12:14:02 +0530

    oikhiouh

--- src/context.jsx
+++ src/context.jsx
@@ -20,7 +20,7 @@ const AppProvider = ({ children }) => {
       type: "HOME_UPDATE",
       payload: {
         name: "STOCK ARCHERY",
-        image: "https://res.cloudinary.com/dyrn2eg1j/image/upload/v1724152404/h44_-_Copy_c8vbgm.png",
+        image: "./images/2.gif",
         paragraph: "Welcome to Stock Archery, your premier source for expert trading knowledge and strategies. Achieve trading success with our in-depth resources, tips, and insights tailored for savvy investors. Start mastering the market today",
       },
     });
